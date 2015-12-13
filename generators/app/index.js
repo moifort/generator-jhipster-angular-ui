7b43b063ba3bc28b70999293c0d6a979758223e1
Thibaut Mottet <thibaut.mottet@gmail.com>
2015-12-13 23:21:45 +0100

    Refactor

--- generators/app/index.js
+++ generators/app/index.js
@@ -35,10 +35,11 @@ module.exports = yeoman.generators.Base.extend({
                 name: 'modules',
                 message: 'Which module do you like to install on your JHipster application?',
                 choices: [
+                    {name: 'Install all modules', value: 'all'},
                     {name: 'Font Awesome', value: 'fontAwesome'},
-                    {name: 'Awesome Checkbox (+Font Awesome)', value: 'awesomeCheckbox'},
+                    {name: 'Awesome Bootstrap Checkbox (+Font Awesome)', value: 'awesomeBootstrapCheckbox'},
                     {name: 'Switchery', value: 'switchery'},
-                    {name: 'slider', value: 'slider'}
+                    {name: 'Angular Bootstrap Slider', value: 'angularBootstrapSlider'}
                 ],
                 default: 'none'
             }
@@ -67,20 +68,39 @@ module.exports = yeoman.generators.Base.extend({
             return;
         }
 
-        if (this.modules.indexOf('fontAwesome') != -1) {
+        // Before install
+        var installAll = (this.modules.indexOf('all') != -1);
+        var installFontAwesome = (this.modules.indexOf('awesomeBootstrapCheckbox') != -1);
+
+        // Install
+        this.buildFontAwesomeSample = false;
+        if (installAll || installFontAwesome || this.modules.indexOf('fontAwesome') != -1) {
             jhipsterFunc.addBowerDependency('font-awesome', '4.5.0');
             jhipsterFunc.addBowerOverride('font-awesome', ['css/font-awesome.css']);
+            this.buildFontAwesomeSample = true;
         }
 
-        if (this.modules.indexOf('awesomeCheckbox') != -1) {
+        this.buildAwesomeBootstrapCheckboxSample = false;
+        if (installAll || this.modules.indexOf('awesomeBootstrapCheckbox') != -1) {
             jhipsterFunc.addBowerDependency('awesome-bootstrap-checkbox', '0.3.5');
+            this.buildAwesomeBootstrapCheckboxSample = true;
         }
 
-        if (this.modules.indexOf('switchery') != -1) {
+        this.buildSwitcherySample = false;
+        if (installAll || this.modules.indexOf('switchery') != -1) {
             jhipsterFunc.addBowerDependency('ng-switchery', '1.0.0-alpha7');
             jhipsterFunc.addAngularJsModule('NgSwitchery');
+            this.buildSwitcherySample = true;
         }
 
+        this.buildAngularBootstrapSliderSample = false;
+        if (installAll || this.modules.indexOf('angularBootstrapSlider') != -1) {
+            jhipsterFunc.addBowerDependency('angular-bootstrap-slider', '0.1.21');
+            jhipsterFunc.addAngularJsModule('ui.bootstrap-slider');
+            this.buildAngularBootstrapSliderSample = true;
+        }
+
+        // Sample page
         this.template('src/main/webapp/scripts/app/angular-ui/_angular-ui.controller.js', webappDir + 'scripts/app/angular-ui/angular-ui.controller.js');
         jhipsterFunc.addJavaScriptToIndex('app/angular-ui/angular-ui.controller.js');
         this.template('src/main/webapp/scripts/app/angular-ui/_angular-ui.html', webappDir + 'scripts/app/angular-ui/angular-ui.html');
